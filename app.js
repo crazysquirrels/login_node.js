b8ba780b46c48bfad0ae4bbe75d6efc9f7ea1e21
crazysquirrels <kew103307@gmail.com>
2017-07-23 15:51:34 -0700

    connect to database

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use('/public', static(path.join(__dirname, 'public')));
 app.use('/uploads', static(path.join(__dirname, 'uploads')));
 
 app.use(cookieParser());
-app.use(expressionSession({
+app.use(expressSession({
     secret: 'my key',
     resave: true,
     saveUninitialized: true
@@ -43,20 +43,22 @@ function connectDB(){
     });
 }
 
-var router.route('/process/login').post(function(req, res){
+var router = express.Router();
+
+router.route('/process/login').post(function(req, res){
     console.log('/process/login - called');
     
     var paramId = req.body.id || req.query.id;
     var paramPassword = req.body.password || req.query.password;
     
-    console.log('login information :' +paramId + paramPassword);
+    console.log('login information :' +'<ID>' +paramId + '<PW>'+ paramPassword);
     
     if(database) {
         authUser(database, paramId, paramPassword, function(err, docs){
             if(err) {throw err;}
             
             if(docs) {
-                console.dir('docs');
+                console.dir(docs);
                 
                 var name = docs[0].name;
                 
@@ -67,17 +69,16 @@ var router.route('/process/login').post(function(req, res){
                 res.write("<br><br><a href='/public/login.html'>Back to Login page</a>");
                 res.end();
             } else
-                res.writeHead('200', {'Content-Type' : 'text/html'; charset =utf-8});
+                res.writeHead('200', {'Content-Type' : 'text/html; charset =utf-8'});
                 res.write('<h2>Failled to login in</h2>')
                 res.write('<div><p>Please check your ID/PW</p></div>');
                 res.write("<br><br><a href='/public/login.html'>Back to Login page</a>");
                 res.end();
-            }
-        });
+            });
     }else {
-                res.writeHead('200', {'Content-Type' : 'text/html'; charset =utf-8});
+                res.writeHead('200', {'Content-Type' : 'text/html; charset =utf-8'});
                 res.write('<h2>Failed connecting Database</h2>')
-                res.write("<br><br><a href='/public/login.html'>Back to Login page</a>");
+                res.write("<div><p>Cannot connect to Database</p></div>");
                 res.end();
     }
 });
@@ -106,4 +107,26 @@ var authUser = function(database, id, password, callback) {
     });
     
 }
+
+app.use('/', router);
+
+app.all('*', function(req, res) {
+	res.status(404).send('<h1>ERROR</h1>');
+});
+
+var errorHandler = expressErrorHandler({
+    static: {
+        '404' : './public/404.html'
+    }
+});
+
+app.use(expressErrorHandler.httpError(404));
+app.use(errorHandler);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+
+    connectDB();
+
+});
 
