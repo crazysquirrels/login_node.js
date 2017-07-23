06781ac0c01b08651e59c28ed3280bc6ae2eab46
crazysquirrels <kew103307@gmail.com>
2017-07-23 11:56:02 -0700

    processing login request

--- app.js
+++ app.js
@@ -11,11 +11,6 @@ var expressErrorHandler = require('express-error-handler');
 
 var expressSession = require('express-session');
 
-var multer = require('multer');
-var fs = require('fs');
-
-var cors = require(cors);
-
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
@@ -33,80 +28,82 @@ app.use(expressionSession({
     saveUninitialized: true
 }));
 
-var router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
+var database;
 
-router.route('/process/login').post(function(req, res){
-    console.log('/process/login processed');
+function connectDB(){
+    var databaseUrl = 'mongodb://localhost:27017/local';
+    
+    MongoClient.connect(databaseUrl, function(err, db){
+        if(err) throw err;
+        
+        console.log('connected to Database: ' + databaseUrl);
+        database =db;   
+    
+    });
+}
+
+var router.route('/process/login').post(function(req, res){
+    console.log('/process/login - called');
     
     var paramId = req.body.id || req.query.id;
     var paramPassword = req.body.password || req.query.password;
     
-    if(req.session.user){
-        console.log('move to login page - already logged in');
-        
-        res.redirect('/public/product.html');
-    }else{
-        req.session.user = {
-            id: paramId,
-            name: 'girls generation',
-            authorized: true
-        };
-        
-        res.writeHead('200', {'Content-Type': 'text/html; chatset=utf-8'});
-        res.write('<h1>Express Server</h1>');
-        res.write('<div><p>Param Id :' +paramId+ '</p></div>');
-        res.write('<div><p>Param Password :' +paramPassword+ '</p></div>');
-        res.write("<br><br><a href='/public/login.html'>Back to Login page</a>");
-        res.end();
-        
-    }
+    console.log('login information :' +paramId + paramPassword);
     
-});
-
-router.route('/process/logout').get(function(req, res){
-    console.log('/process/logout - called');
-    
-    if(req.session.user){
-        console.log('proceed to logout');
-        
-        req.session.destroy(function(err){
-            if(err){throw err;}
+    if(database) {
+        authUser(database, paramId, paramPassword, function(err, docs){
+            if(err) {throw err;}
             
-            console.log('session deleted and logged out');
-            res.redirect('/public/login.html');
+            if(docs) {
+                console.dir('docs');
+                
+                var name = docs[0].name;
+                
+                res.writeHead('200', {'Content-Type': 'text/html; charset=utf-8'});
+                res.write('<h1>Sucessfully logged in</h1>');
+                res.write('<div><p>Param Id :' +paramId+ '</p></div>');
+                res.write('<div><p>Param Password :' +username+ '</p></div>');
+                res.write("<br><br><a href='/public/login.html'>Back to Login page</a>");
+                res.end();
+            } else
+                res.writeHead('200', {'Content-Type' : 'text/html'; charset =utf-8});
+                res.write('<h2>Failled to login in</h2>')
+                res.write('<div><p>Please check your ID/PW</p></div>');
+                res.write("<br><br><a href='/public/login.html'>Back to Login page</a>");
+                res.end();
+            }
         });
-    }else{
-        
-        console.log('need to be logged in');
-        res.redirect('/public/login.html');
-    }
-});
-
-router.route('process/product').get(function(req, res){
-    console.log('/process/product - called');
-    
-    if(req.session.user){
-        res.redirect('/public/product.html');
-    }else{
-        res.redirect('/public/login.html');
+    }else {
+                res.writeHead('200', {'Content-Type' : 'text/html'; charset =utf-8});
+                res.write('<h2>Failed connecting Database</h2>')
+                res.write("<br><br><a href='/public/login.html'>Back to Login page</a>");
+                res.end();
     }
 });
 
 app.use('/', router);
 
-app.all('*', function(req, res) {
-	res.status(404).send('<h1>ERROR</h1>');
-});
+var authUser = function(database, id, password, callback) {
+    console.log('authUser -called');
+    
+    var users = database.collection('users');
+    
+    users.find({"id" : id, "password" : password}).toArray(function(err, docs) {
+        if(err) {
+            callback(err, null);
+            return;
+        }
+        
+        if(docs.length > 0){
+            console.log('Found the user matching ID/PW', id, password);
+            callback(null, docs);
+    
+        }else {
+            console.log('No user with input ID/PW');
+            callback(null, null);
+        }
+    });
+    
+}
 
-var errorHandler = expressErrorHandler({
-    static: {
-        '404' : './public/404.html'
-    }
-});
-
-app.use(expressErrorHandler.httpError(404));
-app.use(errorHandler);
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
